e63ce9026a69d06416505715bdab1450af315c44
shettypratiksha39 <shettypratiksha39@gmail.com>
2019-09-28 15:43:11 +0530

    commit

---
+++ pattern.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+{
+int i,n,j,k,row;
+printf("enter the value of n");
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+for(j=row;j>1;j--)
+{
+printf(" ");
+}}
+for(k=1;k<=i;k++)
+{
+printf("*");
+printf("\n");
+}
+}
+
